d8271e43d4be89758f313d73f622904ecf1c0328
ParkDalJae <inno.over931@gmail.com>
2023-10-30 18:26:49 +0900

    filter 연습

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const a = 1;
-const b = 2;
-console.log(a, b);
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const copyNumbers = numbers.filter((num, index, array) => {
+  return num >= 3 && num <= 5;
+});
+
+console.log(copyNumbers);
+
+const words = ["hello", "world", "hello world", "impossible", "i am", "a person"].filter(
+  (word, index) => {
+    // return word.length > 5; //5음절 이상
+    return index % 2 === 1; // 홀수 배열
+  }
+);
+
+console.log(words);
