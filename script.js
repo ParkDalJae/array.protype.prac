c91dcec554e206eea38c65f190ce31c49593377b
ParkDalJae <124133588+ParkDalJae@users.noreply.github.com>
2023-11-05 23:21:28 +0900

    filter, map, find 예제

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7];
-const copyNumbers = numbers.filter((num, index, array) => {
-  return num >= 3 && num <= 5;
-});
+//filter
+// 숫자 배열에서 짝수만 필터링
+const numbers = [1, 2, 3, 4, 5, 6];
+const evenNumbers = numbers.filter((num) => num % 2 === 0);
+console.log(evenNumbers); // [2, 4, 6]
 
-console.log(copyNumbers);
+// 문자열 배열에서 길이가 5 이상인 단어들 필터링
+const words = ["apple", "banana", "cherry", "date", "elderberry"];
+const longWords = words.filter((word) => word.length >= 5);
+console.log(longWords); // ["apple", "banana", "cherry", "elderberry"]
 
-const words = ["hello", "world", "hello world", "impossible", "i am", "a person"].filter(
-  (word, index) => {
-    // return word.length > 5; //5음절 이상
-    return index % 2 === 1; // 홀수 배열
-  }
-);
+//map
+// 숫자 배열의 각 원소 제곱하여 새로운 배열 생성
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers.map((num) => num * num);
+console.log(squaredNumbers); // [1, 4, 9, 16, 25]
 
-console.log(words);
+// 객체 배열에서 이름만 추출하여 새로운 배열 생성
+const people = [{ name: "Alice", age: 30 }, { name: "Bob", age: 25 }, { name: "Carol", age: 35 }];
+const names = people.map((person) => person.name);
+console.log(names); // ["Alice", "Bob", "Carol"]
+
+
+//find
+
+// 배열에서 첫 번째로 3의 배수인 숫자 찾기
+const numbers = [2, 4, 6, 9, 12];
+const firstMultipleOf3 = numbers.find((num) => num % 3 === 0);
+console.log(firstMultipleOf3); // 6
+
+// 객체 배열에서 특정 조건을 만족하는 첫 번째 객체 찾기
+const people = [{ name: "Alice", age: 30 }, { name: "Bob", age: 25 }, { name: "Carol", age: 35 }];
+const personOver30 = people.find((person) => person.age > 30);
+console.log(personOver30); // { name: "Carol", age: 35 }
