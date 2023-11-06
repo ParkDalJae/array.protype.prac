279441e799a538dee2c33598212eeae1eb0a060b
ParkDalJae <inno.over931@gmail.com>
2023-11-06 10:35:31 +0900

    중복선언 수정

--- array.js
+++ array.js
@@ -1,8 +1,8 @@
 //filter
 //1
 // 숫자 배열에서 짝수만 필터링
-const numbers = [1, 2, 3, 4, 5, 6];
-const evenNumbers = numbers.filter((num) => num % 2 === 0);
+const numbers1 = [1, 2, 3, 4, 5, 6];
+const evenNumbers = numbers1.filter((num) => num % 2 === 0);
 console.log(evenNumbers); // [2, 4, 6]
 
 //2
@@ -14,48 +14,55 @@ console.log(longWords); // ["apple", "banana", "cherry", "elderberry"]
 //map
 //1
 // 숫자 배열의 각 원소 제곱하여 새로운 배열 생성
-const numbers = [1, 2, 3, 4, 5];
-const squaredNumbers = numbers.map((num) => num * num);
+const numbers2 = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers2.map((num) => num * num);
 console.log(squaredNumbers); // [1, 4, 9, 16, 25]
 
 //2
 // 객체 배열에서 이름만 추출하여 새로운 배열 생성
-const people = [{ name: "Alice", age: 30 }, { name: "Bob", age: 25 }, { name: "Carol", age: 35 }];
-const names = people.map((person) => person.name);
+const people1 = [
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 25 },
+  { name: "Carol", age: 35 },
+];
+const names = people1.map((person) => person.name);
 console.log(names); // ["Alice", "Bob", "Carol"]
 
 //find
 //1
 // 배열에서 첫 번째로 3의 배수인 숫자 찾기
-const numbers = [2, 4, 6, 9, 12];
-const firstMultipleOf3 = numbers.find((num) => num % 3 === 0);
+const numbers3 = [2, 4, 6, 9, 12];
+const firstMultipleOf3 = numbers3.find((num) => num % 3 === 0);
 console.log(firstMultipleOf3); // 6
 
 //2
 // 객체 배열에서 특정 조건을 만족하는 첫 번째 객체 찾기
-const people = [{ name: "Alice", age: 30 }, { name: "Bob", age: 25 }, { name: "Carol", age: 35 }];
-const personOver30 = people.find((person) => person.age > 30);
+const people2 = [
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 25 },
+  { name: "Carol", age: 35 },
+];
+const personOver30 = people2.find((person) => person.age > 30);
 console.log(personOver30); // { name: "Carol", age: 35 }
 
 //reduce
 //1
-const numbers = [1, 2, 3, 4, 5];
+const numbers4 = [1, 2, 3, 4, 5];
 
 // 배열의 모든 요소를 합산
-const sum = numbers.reduce((accumulator, current) => accumulator + current, 0);
+const sum = numbers4.reduce((accumulator, current) => accumulator + current, 0);
 
 console.log(sum); // 15
 
 //2
 const orders = [
-    { product: 'Laptop', price: 1000 },
-    { product: 'Smartphone', price: 500 },
-    { product: 'Tablet', price: 300 },
-    { product: 'Headphones', price: 100 },
-  ];
-  
-  // 주문 목록의 총 가격 계산
-  const totalCost = orders.reduce((total, order) => total + order.price, 0);
-  
-  console.log(totalCost); // 1900
-  
+  { product: "Laptop", price: 1000 },
+  { product: "Smartphone", price: 500 },
+  { product: "Tablet", price: 300 },
+  { product: "Headphones", price: 100 },
+];
+
+// 주문 목록의 총 가격 계산
+const totalCost = orders.reduce((total, order) => total + order.price, 0);
+
+console.log(totalCost); // 1900
